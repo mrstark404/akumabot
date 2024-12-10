df3ffd89d93707e1c6e92743173644bd5aeedb9f
mrstark404 <112131793+mrstark404@users.noreply.github.com>
2024-12-10 14:22:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-import configparser
 from dotenv import load_dotenv
 from os import getenv
 
@@ -11,10 +10,8 @@ DATABASE_URL = getenv('DATABASE_URL')
 SESSION = getenv('STRING_SESSION')
 SRC_ID = int(getenv('SRC_ID'))
 DST_ID = int(getenv('DST_ID'))
-FROM_MSG = int(getenv('FROM_MSG'))
-BATCH_SIZE = int(getenv('BATCH_SIZE'))
-MAX_ATTEMPTS = int(getenv('MAX_ATTEMPTS'))
+FROM_MSG = int(getenv('FROM_MSG', 0))
+BATCH_SIZE = int(getenv('BATCH_SIZE', 10))
+MAX_ATTEMPTS = int(getenv('MAX_ATTEMPTS', 15))
 
-if __name__ == '__main__':
-    pass
 
